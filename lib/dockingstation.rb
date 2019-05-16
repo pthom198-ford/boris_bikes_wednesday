064892e3cfaf7faf98a1756d4ec745a063619d93
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-05-16 10:13:17 +0100

    add_bike method

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -2,8 +2,25 @@
 
 class DockingStation
 
-  def release_bike
-   return Bike.new
+  attr_reader :dock
+
+
+  def initialize
+    @dock = []
   end
+
+  # def release_bike
+  #  return Bike.new
+  # end
+
+
+
+  def add_bike(bike)
+   @dock.push(bike)
+  end
+
+  # def view_bike
+  #
+  # end
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,21 +1,29 @@
 require './lib/dockingstation'
 require './lib/bike'
 
-describe DockingStation do
-  it 'can release bike from docking station' do
-    docking_station = DockingStation.new
-    expect(docking_station.release_bike).to be_an_instance_of(Bike)
-  end
 
-  it 'expects the bike to be working' do
-    docking_station = DockingStation.new
-    released_bike = docking_station.release_bike
-    expect(released_bike.working?).to eq(true)
-  end
+describe DockingStation do
+  # it 'can release bike from docking station' do
+  #   docking_station = DockingStation.new
+  #   expect(docking_station.release_bike).to not_include(bike)
+  # end
+  #
+  # it 'expects the bike to be working' do
+  #   docking_station = DockingStation.new
+  #   released_bike = docking_station.release_bike
+  #   expect(released_bike.working?).to eq(true)
+  # end
 
   it 'expects to return the bike to docking station' do
     docking_station = DockingStation.new
-    expect(docking_station.return_bike).to eq(true)
+    bike = Bike.new
+    docking_station.add_bike(bike)
+    expect(docking_station.dock).to include(bike)
   end
 
+  # it 'expects to see a bike in docking station' do
+  #   docking_station = DockingStation.new
+  #   docking_station.add_bike
+  #   expect(docking_station.view_dock).to include(bike)
+  # end
 end
