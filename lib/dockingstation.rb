402713f12074b9c63ec52a019a50cb0530c811f4
Egle Bernotaite <ebernotaite94@gmail.com>
2019-05-16 15:17:03 +0100

    Refactor

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -4,23 +4,20 @@ class DockingStation
 
   attr_reader :dock
 
-
   def initialize
     @dock = []
   end
 
-  # def release_bike
-  #  return Bike.new
-  # end
-
-
+  def release_bike
+   @dock.pop
+  end
 
   def add_bike(bike)
    @dock.push(bike)
   end
 
-  # def view_bike
-  #
-  # end
+  def view_dock
+    @dock
+  end
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,27 +3,23 @@ require './lib/bike'
 
 
 describe DockingStation do
-  # it 'can release bike from docking station' do
-  #   docking_station = DockingStation.new
-  #   expect(docking_station.release_bike).to not_include(bike)
-  # end
-  #
-  # it 'expects the bike to be working' do
-  #   docking_station = DockingStation.new
-  #   released_bike = docking_station.release_bike
-  #   expect(released_bike.working?).to eq(true)
-  # end
+
+  let(:bike){Bike.new}
+  let(:docking_station){DockingStation.new}
+
+  it 'can release bike from docking station' do
+    docking_station.add_bike(bike)
+    docking_station.release_bike
+    expect(docking_station.dock).not_to include(bike)
+  end
 
   it 'expects to return the bike to docking station' do
-    docking_station = DockingStation.new
-    bike = Bike.new
     docking_station.add_bike(bike)
     expect(docking_station.dock).to include(bike)
   end
 
-  # it 'expects to see a bike in docking station' do
-  #   docking_station = DockingStation.new
-  #   docking_station.add_bike
-  #   expect(docking_station.view_dock).to include(bike)
-  # end
+  it 'expects to see a bike in docking station' do
+    docking_station.add_bike(bike)
+    expect(docking_station.dock).to include(bike)
+  end
 end
