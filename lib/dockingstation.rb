c70b2cfef851d9ed4e27b97558b194338069e910
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-05-16 16:37:35 +0100

    refactoring with Egle

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -9,15 +9,20 @@ class DockingStation
   end
 
   def release_bike
-   @dock.pop
+#@dock.empty? is equivalent to @dock != nil
+    empty? ? raise("aint no bike fool") : @dock.pop
   end
 
   def add_bike(bike)
-   @dock.push(bike)
+     full? ? raise("theres a bike here fool") : @dock.push(bike)
   end
 
-  def view_dock
-    @dock
-  end
+  private
+  def full?
+    @dock.count == 20
+ end
 
+  def empty?
+    @dock.count == 0
+  end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -22,4 +22,17 @@ describe DockingStation do
     docking_station.add_bike(bike)
     expect(docking_station.dock).to include(bike)
   end
+
+  it 'throws an error when there are no bikes available' do
+    expect{docking_station.release_bike}.to raise_error("aint no bike fool")
+  end
+
+  it 'throws an error when there is a bike already docked' do
+    20.times{docking_station.add_bike(bike)}
+    expect{docking_station.add_bike(bike)}.to raise_error("theres a bike here fool")
+  end
+  it "can have a capacity of 20 bikes" do
+    20.times{docking_station.add_bike(bike)}
+    expect(docking_station.dock.count).to eq(20)
+  end
 end
